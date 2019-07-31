3ef79f2c4b007d694d2c9cb42e7cd3d0ee16e5e8
Kristophe <kristophe@pennacool.com>
2019-07-31 15:20:57 -0400

    add player and movement

--- src/scenes/SimpleScene.js
+++ src/scenes/SimpleScene.js
@@ -1,13 +1,81 @@
 import Phaser from "phaser";
 
 class SimpleScene extends Phaser.Scene {
+  constructor() {
+    super();
+    this.platforms = null;
+    this.player = null;
+    this.cursors = null;
+  }
+
   preload() {
-    this.load.image("cokecan", "assets/images/cokecan.png");
+    this.load.image("sky", "assets/images/sky.png");
+    this.load.image("ground", "assets/images/platform.png");
+    this.load.image("star", "assets/images/star.png");
+    this.load.image("bomb", "assets/images/bomb.png");
+    this.load.spritesheet("dude", "assets/images/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48
+    });
   }
 
   create() {
-    this.add.text(100, 100, "Hello Phaser!", { fill: "#0f0" });
-    this.add.image(100, 200, "cokecan");
+    this.add.image(400, 300, "sky");
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms
+      .create(400, 568, "ground")
+      .setScale(2)
+      .refreshBody();
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
+
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, this.platforms);
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
 
