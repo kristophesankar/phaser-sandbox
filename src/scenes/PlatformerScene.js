f2873f8420d9d7e8961eb4ae77815aec10b40a36
Kristophe <kristophe.lukas.sankar@gmail.com>
2019-08-13 16:30:49 -0400

    bugfix: update phaser

--- src/scenes/PlatformerScene.js
+++ src/scenes/PlatformerScene.js
@@ -1,44 +1,48 @@
-import { Scene, Display } from "phaser";
+import Phaser, { Scene, Display } from "phaser";
 
 class PlatformerScene extends Scene {
   constructor(args) {
     super();
-    Scene.call(this, { key: 'sceneB' });
+    Scene.call(this, { key: "sceneB" });
     this.map;
   }
 
-  preload () {
+  preload() {
     this.load.image("sky", "./assets/images/sky.png");
     this.load.image("tiles", "assets/images/platformPack_tilesheet.png");
-    this.load.tilemapTiledJSON("map", "assets/json/newmap.json");
+    this.load.tilemapTiledJSON("map", "assets/json/groundmap.json");
     this.load.spritesheet("dude", "assets/images/dude.png", {
       frameWidth: 32,
       frameHeight: 48
     });
   }
 
-  create () {
+  create() {
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("platformPack_tilesheet", "tiles");
 
     // Parameters: layer name (or index) from Tiled, tileset, x, y
-    const backgroundLayer = map.createStaticLayer("Background", tileset, 0, 0);
     const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
     worldLayer.setCollisionByProperty({ collides: true });
 
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
-    worldLayer.renderDebug(debugGraphics, {
+    // Turn on physics debugging to show player's hitbox
+    console.log(this.physics.world.createDebugGraphic());
+    // debugger;
+    this.physics.world.createDebugGraphic();
+
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    worldLayer.renderDebug(graphics, {
       tileColor: null, // Color of non-colliding tiles
-      collidingTileColor: new Display.Color(243, 134, 48, 255), // Color of colliding tiles
-      faceColor: new Display.Color(40, 39, 37, 255) // Color of colliding face edges
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
     });
   }
 
-  upload () {
-
-  }
+  upload() {}
 }
 
 export default PlatformerScene;
-
-
